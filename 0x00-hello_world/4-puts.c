a8e84dd6c956cc59c9caa6f05ad53161cf23aecd
kalukwo <kaldrack789@gmail.com>
2023-02-20 04:32:21 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function
- * Return : 0(succes)
+ * main - a program that prints a line using the  puts function
+ * Return: 0 (succes)
  */
 int main(void)
 {
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *  * main - a  program that prints the size of various types on the computer it is compiled and run on
- *   * return: 0 (success)
- *    */
+ * main - Entry point 
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	        char a;
